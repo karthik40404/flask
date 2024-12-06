c7a37b6c5d1d6ba80f5d5ede61796eb0bc845c77
karthik40404 <karpython123@gmail.com>
2024-12-06 11:30:48 +0530

    flaskform

--- flask_pro/home.py
+++ flask_pro/home.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template ,request,redirect,url_for
 app = Flask(__name__)
-
+import sqlite3
+data =[]
 @app.route('/')
 def f1():
     return 'welcome'
@@ -9,9 +10,25 @@ def f1():
 def f2():
     return 'welcome all'
 
-@app.route('/index')
-def f3():
-    return render_template('index1.html')
+@app.route('/ind', methods=['POST', 'GET'])
+def fun4():
+    con = sqlite3.connect('database.db') 
+    try:
+        con.execute("create table user(name text, age int)")
+    except:
+        pass   
+    if request.method == 'POST':
+        name = request.form.get('name')
+        age = request.form.get('age')
+        if name and age:
+            data.append({"name": name, "age": age})
+        con.execute("INSERT INTO user(age,name) VALUES (?, ?)", (int(age), name))
+        con.commit()
+        con.close()      
+        return redirect(url_for('fun4'))
+    else:
+        con.close()
+        return render_template('index1.html',data=data)
 
 @app.route('/p')
 def f4():
